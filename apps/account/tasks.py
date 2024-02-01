234cd14caf112dbb2b41f13d149f438b012e1082
Myrza <loadenggg@gmail.com>
2024-02-01 16:55:50 +0600

    add activate url

--- apps/account/tasks.py
+++ apps/account/tasks.py
@@ -5,7 +5,7 @@ from config.celery import app
 
 @app.task()
 def send_confirmation_email(email, code):
-    activation_url = f'http://127.0.0.1:8000/account/api/account/activate/?u={code}'
+    activation_url = f'http://13.60.9.9/account/api/account/activate/?u={code}'
 
     message = format_html(
         '<h2>Hello, activate your account!</h2>\n'
