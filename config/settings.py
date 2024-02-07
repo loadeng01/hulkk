0da7127dda21950ecc16956047be55f7e3ad7b09
Myrza <loadenggg@gmail.com>
2024-02-07 21:11:58 +0600

    qwerty ples

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,14 @@ CORS_ALLOWED_ORIGINS = [
     'https://cmsup.net.kg'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://cmsup.net.kg'
+    'https://cmsup.net.kg'
+]
+
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
